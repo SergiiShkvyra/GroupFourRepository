55634b00e7cef76ffdad14f017912cb6cc3d6929
HassanOua <65350081+HassanOua@users.noreply.github.com>
2020-05-17 22:46:04 -0400

    Adding More Coding

--- src/noorteck/TestClass.java
+++ src/noorteck/TestClass.java
@@ -1,6 +1,40 @@
 package noorteck;
 
 public class TestClass {
+	int age;
+	String name;
+	String batch;
+
+	public TestClass(int age, String name, String batch) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.batch = batch;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBatch() {
+		return batch;
+	}
+
+	public void setBatch(String batch) {
+		this.batch = batch;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Branching practice");
